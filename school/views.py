734e0c73b7c4af6769022663290784a00f3a0cb0
AJlekCandr_proff <svznikov@yandex.ru>
2024-12-17 23:26:28 +0300

    feat: Create function for vies main page.

--- school/views.py
+++ school/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request) -> HttpResponse:
+    """
+    Функция представления для главной страницы.
+
+    :param request:
+
+    :return: Объект класса HttpResponse.
+    """
+
+    return render(request, 'school/index.html')
