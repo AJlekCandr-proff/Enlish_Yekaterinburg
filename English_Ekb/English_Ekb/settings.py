9e853e5118e7e2e0a4426593d727264b0a94bba8
AJlekCandr_proff <svznikov@yandex.ru>
2024-12-17 23:45:41 +0300

    feat: Update urls.py

--- English_Ekb/English_Ekb/settings.py
+++ English_Ekb/English_Ekb/settings.py
@@ -58,7 +58,7 @@ JAZZMIN_SETTINGS = {
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
