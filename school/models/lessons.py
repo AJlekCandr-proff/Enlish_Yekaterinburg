482ad83fd596f22bc60509125feb74f97693538d
AJlekCandr_proff <svznikov@yandex.ru>
2024-12-19 17:24:59 +0300

    feat: Add model table with lessons.

--- school/models/lessons.py
+++ school/models/lessons.py
@@ -1,5 +1,22 @@
-Дата;
-Время урока (длительность);
-Необходимые материалы (презентации и т.п.);
-Преподаватель;
-Ученик (ученики);
+from django.db.models import Model, DateField, FileField, CharField, URLField, ManyToManyField
+from django.utils import timezone
+
+from .students import Students
+
+
+class Lessons(Model):
+    title = CharField(max_length=25, verbose_name='Тема урока')
+    date = DateField(verbose_name='Дата урока', default=timezone.now())
+    optional_material = FileField(verbose_name='Материал к уроку')
+    teacher_name = CharField(verbose_name='ФИО предподавателя')
+    link_to_lesson = URLField(verbose_name='Ссылка на урок (конференцию)')
+    pupils = ManyToManyField(Students, related_name='lessons', verbose_name='Ученики')
+
+    class Meta:
+        db_table_comment = 'Table with lessons.'
+        db_table = 'lessons'
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
+
+    def __str__(self) -> str:
+        return self.title
