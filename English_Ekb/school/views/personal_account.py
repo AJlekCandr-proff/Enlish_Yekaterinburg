19da45b3063d53210b9f1facda811be6fcad1d71
AJlekCandr_proff <svznikov@yandex.ru>
2024-12-26 22:53:01 +0300

    feat: Create views for loging and registration.

--- English_Ekb/school/views/personal_account.py
+++ English_Ekb/school/views/personal_account.py
@@ -25,3 +25,27 @@ def account(request: HttpRequest) -> HttpResponse:
         template_name='school/pages/account.html',
         context={'user_profile': user_profile, 'user_lessons': user_lessons}
     )
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    """
+    Функция представления для входа в личный кабинет студента или предподавателя.
+
+    :param request: Объект класса HttpRequest.
+
+    :return: Объект класса HttpResponse.
+    """
+
+    return render(request=request, template_name='school/pages/login.html')
+
+
+def registration(request: HttpRequest) -> HttpResponse:
+    """
+    Функция представления для регистрации аккаунта нового студента.
+
+    :param request: Объект класса HttpRequest.
+
+    :return: Объект класса HttpResponse.
+    """
+
+    return render(request=request, template_name='school/pages/registration.html')
