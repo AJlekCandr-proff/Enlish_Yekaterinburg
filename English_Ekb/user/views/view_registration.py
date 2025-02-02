7b6877e4003ffe3957d08f12f9ca5fcff871b6cd
AJlekCandr_proff <svznikov@yandex.ru>
2025-02-02 21:42:46 +0300

    feat: Update .env-file and add sending email to user.

--- English_Ekb/user/views/view_registration.py
+++ English_Ekb/user/views/view_registration.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
+from django.core.mail import send_mail
 from django.views.generic import CreateView
+from django.conf import settings
 
 from ..forms import RegistrationUserForm
 
@@ -9,6 +11,19 @@ class RegistrationUserView(CreateView):
 
     template_name = 'user/pages/registration.html'
     extra_context = {'title': 'Регистрация нового пользователя'}
+
+    def form_valid(self, form):
+        user = form.save()
+
+        subject = "Welcome to Our Site!"
+        message = f"Thank you for registering, {user.username}!"
+
+        from_email = settings.DEFAULT_FROM_EMAIL
+        recipient_list = [user.email]
+
+        send_mail(subject, message, from_email, recipient_list)
+
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse_lazy('user:registration-confirm')
